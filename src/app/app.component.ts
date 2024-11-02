1960fe847646492a06724dd0acd1ca02ab2a094b
Vigitoreanec <chirik39@mail.ru>
2024-11-03 00:03:12 +0200

    Доделал связывание и удаление

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'routing-app-TODO';
+  todos: any[] = [];
+  idCreate = Math.random();
+  styleSpan = 'text-decoration: line-through;';
+  onChecked: boolean = false;
+
+  addTodo(newTodo: string) {
+    this.todos.push({ id: this.todos.length, text: newTodo, checked: this.onChecked });
+    console.debug(this.todos);
+  }
+
+  isCompleted() {
+    this.onChecked = !this.onChecked;
+  }
+
+  deleteTodo(id: number) {
+    this.todos.splice(id, 1);
+  }
 }
